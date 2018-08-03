caea39aa41a77dda933e436383f002f52fa6c794
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-03 15:37:31 -0700

    Initial Commit

---
+++ Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Jamie on 2018-08-03.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
